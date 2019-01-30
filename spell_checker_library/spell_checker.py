87f0358dd5c013048eda57b214f36cde90e32370
Timur Nurlygayanov <timurnurlygayanov@mail.ru>
2019-01-30 17:09:41 +0200

    Added new classificator

--- spell_checker_library/spell_checker.py
+++ spell_checker_library/spell_checker.py
@@ -23,16 +23,25 @@ import pymorphy2
 from tqdm import tqdm
 
 
+# TODO:
+# Create our own dictionary based on the text messages in chat.
+# Not only replace based on neural network, but also based on
+# simple dictionary.
+
 class Checker(object):
 
     model = None
     words_dict = []
+    learned_words_dict = []
     model_name = 'ru_spellcheck.model'
     tags = {'NOUN', 'ADJF', 'ADJS', 'VERB', 'INFN',
             'PRTF', 'PRTS', 'GRND'}
 
-    def check_word_in_dict(self, word):
+    def check_word_in_dict(self, word, ignore='[A-Za-z0-9]'):
         """ Check only one word in the dict. """
+
+        if re.search(ignore, word):
+            return True
 
         if not self.words_dict:
             # Download Russian dictionary:
@@ -49,15 +58,19 @@ class Checker(object):
     def text_to_word_sequence(self, text):
         """ Makes the array of words from the text. """
 
-        text = re.sub("[!@#$%^&*()[]{};:,./<>?\|`~-=_+ ]", '\n', text)
+        ignore_chars = r"[!@#$%^&*(){};:,./<>?\|`~=-_+]"
 
-        result = [str(word).strip() for word in text.split('\n') if word]
+        text = re.sub(ignore_chars, ' ', text)
+
+        result = [str(word).strip() for word in text.split(' ')
+                  if word and word not in ignore_chars]
 
         return result
 
     def learn(self, text):
         """ Learn sentences patterns based on some text. """
 
+        text = str(text).lower()
         sentences = []
 
         # Normalization
@@ -67,13 +80,17 @@ class Checker(object):
         morph = pymorphy2.MorphAnalyzer()
 
         for i in tqdm(range(len(sentences))):
-
-            word = sentences[i]
             sentence = []
 
-            p = morph.parse(word)[0]
-            if p.tag.POS in self.tags:
-                sentence.append(p.normal_form)
+            for word in sentences[i]:
+                p = morph.parse(word)[0]
+                if p.tag.POS in self.tags:
+                    sentence.append(p.normal_form)
+
+                if text.count(word) > 5:
+                    word = word.strip()
+                    if word not in self.learned_words_dict:
+                        self.learned_words_dict.append(word)
 
             sentences[i] = sentence
         sentences = [x for x in sentences if x]
@@ -81,11 +98,13 @@ class Checker(object):
         # Training the model (it can take 2+ hours):
         model = gensim.models.FastText(sentences, size=300,
                                        window=4, min_count=3, sg=1,
-                                       iter=100, min_n=1, max_n=8)
+                                       iter=100, min_n=3, max_n=6)
         model.init_sims(replace=True)
 
         # Save model to the disk (it will take 200+ Mb on disk)
         model.save(self.model_name)
+
+        self.words_dict += self.learned_words_dict
 
     def spellcheck(self, text):
         """ Correct the text. """
@@ -103,7 +122,16 @@ class Checker(object):
 
         for word in words:
             if not self.check_word_in_dict(word):
-                correct_words = self.model.most_similar(positive=[word])
-                text = text.replace(word, correct_words[0])
+
+                try:
+                    correct_words = self.model.most_similar(positive=[word])
+
+                    if correct_words[0][1] > 0.8:
+                        text = text.replace(' {0} '.format(word),
+                                            ' {0} '.format(str(correct_words[0][0])),
+                                            1)
+                except:
+                    pass  # Ignore any words if model can't find any
+                          # options to replace it.
 
         return text
--- telegram_bot_td-tdf/telegram_bot.py
+++ telegram_bot_td-tdf/telegram_bot.py
@@ -8,10 +8,14 @@
 # Many thanks to the author of this great article!!!
 #
 
+import sys
+sys.path.append('../')
+
 import re
-import os.path
 from telethon.sync import TelegramClient
 from configparser import ConfigParser
+
+from termcolor import colored
 
 import nltk
 import pandas as pd
@@ -22,6 +26,11 @@ from sklearn.cluster import KMeans
 
 import matplotlib.pyplot as plt
 from sklearn.manifold import MDS
+
+
+from spell_checker_library.spell_checker import Checker
+
+my_checker = Checker()
 
 
 # Read Telegram private key from the local file:
@@ -48,7 +57,7 @@ ALL_MESSAGES = []
 ALL_QUESTIONS = []
 
 with TelegramClient(name, api_id, api_hash) as client:
-    for message in client.iter_messages(chat):
+    for message in client.iter_messages(chat, limit=4000):
         if message.text:
             ALL_MESSAGES.append(str(message.text))
 
@@ -59,29 +68,44 @@ for q in ALL_MESSAGES:
     for m in msgs:
         if len(m) > 5:
             if '?' in str(m):
-                ALL_QUESTIONS.append(m)
-
-for q in ALL_QUESTIONS:
-    print(q + "\n====\n")
+                for question in m.split('?')[:-1]:
+                    ALL_QUESTIONS.append(question + '?')
 
 
 print('Total messages: {0}'.format(len(ALL_MESSAGES)))
 print('Questions found: {0}'.format(len(ALL_QUESTIONS)))
 
 
+my_checker.learn('\n'.join(ALL_MESSAGES))
+
+
+for i, q in enumerate(ALL_QUESTIONS):
+    q = str(q).strip()
+    q2 = my_checker.spellcheck(q)
+
+    """
+    if q2 != q:
+        print(colored(q, 'red'))
+        print(colored(q2, 'green'))
+
+        print('\n --- \n\n')
+    """
+
+    ALL_QUESTIONS[i] = q2
+
+# print(my_checker.learned_words_dict)
+# exit(0)
+
+print(len(ALL_QUESTIONS))
+ALL_QUESTIONS = [q for q in ALL_QUESTIONS if ' ' in q]
+print(len(ALL_QUESTIONS))
+print('*'*20)
+
 titles = ALL_QUESTIONS
 synopses = ALL_QUESTIONS
 
 stop_words = nltk.corpus.stopwords.words('russian')
 stemmer = SnowballStemmer('russian')
-
-# Additional stop words:
-stop_words += ['бол', 'больш', 'будт', 'быт', 'вед', 'впроч', 'всег', 'всегд',
-               'даж', 'друг', 'е', 'ег', 'ем', 'есл', 'ест', 'ещ', 'зач', 'зде',
-               'ил', 'иногд', 'когд', 'конечн', 'куд', 'лучш', 'межд', 'мен', 'мног',
-               'мо', 'можн', 'нег', 'нельз', 'нибуд', 'никогд', 'нич', 'опя', 'посл',
-               'пот', 'почт', 'разв', 'сво', 'себ', 'совс', 'теб', 'тепер', 'тог',
-               'тогд', 'тож', 'тольк', 'хорош', 'хот', 'чег', 'чут', 'эт', 'оп', 'а']
 
 
 def tokenize_only(text, correct_symbols='[А-Яа-яЁё]'):
@@ -108,6 +132,7 @@ def tokenize_and_stem(text):
     return stems
 
 
+"""
 totalvocab_stemmed = []
 totalvocab_tokenized = []
 for i in synopses:
@@ -126,7 +151,7 @@ print('\n There are ' + str(vocab_frame.shape[0]) +
       ' items in vocab_frame')
 
 # Define vectorizer parameters:
-tfidf_vectorizer = TfidfVectorizer(max_df=0.95, max_features=200000,
+tfidf_vectorizer = TfidfVectorizer(max_df=1.0, max_features=20000,
                                    min_df=0.01, stop_words=stop_words,
                                    use_idf=True, tokenizer=tokenize_and_stem,
                                    ngram_range=(1, 6))
@@ -137,50 +162,28 @@ tfidf_matrix = tfidf_vectorizer.fit_transform(synopses)
 terms = tfidf_vectorizer.get_feature_names()
 
 dist = 1 - cosine_similarity(tfidf_matrix)
-
-
-num_clusters = 5
-int_previous = 100
-smallest = 100
-
 """
-for num_clusters in range(4, 5):
 
-    km = KMeans(n_clusters=num_clusters)
 
-    km.fit(tfidf_matrix)
+from sklearn.feature_extraction.text import CountVectorizer
+vectorizer = CountVectorizer(tokenizer=tokenize_and_stem,
+                             stop_words=stop_words)
 
-    interia = km.inertia_
-    print("k: ", num_clusters, " cost: ", interia, '  ', (int_previous-interia)/interia)
+X = vectorizer.fit_transform(synopses)
 
-    if (int_previous-interia)/interia > 0.0 and smallest > (int_previous-interia)/interia:
-        smallest = (int_previous-interia)/interia
-
-    int_previous = interia
-
-print(smallest)
-"""
+num_clusters = 10
 
 km = KMeans(n_clusters=num_clusters)
-km.fit(tfidf_matrix)
+km.fit(X)
 
-# Dump the model:
-# joblib.dump(km,  'doc_cluster.pkl')
-# km = joblib.load('doc_cluster.pkl')
-# clusters = km.labels_.tolist()
-
-
-predict_me = tfidf_vectorizer.transform(['Какой провайдер лучше?'])
-print(km.predict(predict_me))
-
-predicted_class = km.predict(predict_me)
+dist = 1 - cosine_similarity(X)
 
 
 print('Similar questions:')
 for i in ALL_QUESTIONS:
-    predict_me = tfidf_vectorizer.transform([i])
-    if km.predict(predict_me) == predicted_class:
-        print(i + '\n=====\n\n')
+    predict_me = vectorizer.transform([i])
+    if km.predict(predict_me) == 2:
+        print(i + '\n=\n\n')
 
 
 clusters = km.labels_.tolist()
@@ -199,14 +202,20 @@ xs, ys = pos[:, 0], pos[:, 1]
 
 
 #set up colors per clusters using a dict
-cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3', 3: '#e7298a', 4: '#66a61e'}
+cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3', 3: '#e7298a', 4: '#66a61e',
+                  5: '#1b9077', 6: '#d95ff2', 7: '#0070b3', 8: '#572980', 9: '#9fa61e'}
 
 #set up cluster names using a dict
-cluster_names = {0: 'Family, home, war',
-                 1: 'Police, killed, murders',
-                 2: 'Father, New York, brothers',
-                 3: 'Dance, singing, love',
-                 4: 'Killed, soldiers, captain'}
+cluster_names = {0: '1',
+                 1: '2',
+                 2: '3',
+                 3: '4',
+                 4: '5',
+                 5: '6',
+                 6: '7',
+                 7: '8',
+                 8: '9',
+                 9: '10'}
 
 
 # create data frame that has the result of the MDS plus the cluster numbers and titles
@@ -216,33 +225,33 @@ df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=titles))
 groups = df.groupby('label')
 
 # set up plot
-fig, ax = plt.subplots(figsize=(17, 9))  # set size
+fig, ax = plt.subplots(figsize=(9, 9))  # set size
 ax.margins(0.05)  # Optional, just adds 5% padding to the autoscaling
 
 # iterate through groups to layer the plot
 # note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
 for name, group in groups:
-    ax.plot(group.x, group.y, marker='o', linestyle='', ms=12,
+    ax.plot(group.x, group.y, marker='o', linestyle='', ms=10,
             label=cluster_names[name], color=cluster_colors[name],
             mec='none')
     ax.set_aspect('auto')
     ax.tick_params( \
         axis='x',  # changes apply to the x-axis
         which='both',  # both major and minor ticks are affected
-        bottom='off',  # ticks along the bottom edge are off
-        top='off',  # ticks along the top edge are off
-        labelbottom='off')
+        bottom=False,  # ticks along the bottom edge are off
+        top=False,  # ticks along the top edge are off
+        labelbottom=False)
     ax.tick_params( \
         axis='y',  # changes apply to the y-axis
         which='both',  # both major and minor ticks are affected
-        left='off',  # ticks along the bottom edge are off
-        top='off',  # ticks along the top edge are off
-        labelleft='off')
+        left=False,  # ticks along the bottom edge are off
+        top=False,  # ticks along the top edge are off
+        labelleft=False)
 
 ax.legend(numpoints=1)  # show legend with only 1 point
 
 # add label in x,y position with the label as the film title
-for i in range(len(df)):
-    ax.text(df.ix[i]['x'], df.ix[i]['y'], df.ix[i]['title'], size=8)
+#for i in range(len(df)):
+#    ax.text(df.ix[i]['x'], df.ix[i]['y'], df.ix[i]['title'], size=8)
 
 plt.show()  # show the plot

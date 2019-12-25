e8eab5cae91851b341861f8746bfb991233dde4e
TimurNurlygayanov <t.nurlygaianov@semrush.com>
2019-12-25 13:23:49 +0200

    Added example of fast text

---
+++ fast_text_example/README.md
@@ -0,0 +1,31 @@
+Introduction
+------------
+
+This is very simple example of how to use
+Gensim library and FastText model to check
+the similarity of different strings.
+
+Note: the strings can be large.
+
+More information can be found here: https://radimrehurek.com/gensim/auto_examples/tutorials/run_fasttext.html
+
+
+How To Install Requirements
+---------------------------
+To install requirements just run the following commands:
+
+    pip3 install -r requirements.txt
+    
+And then run in your Python interpretator:
+
+    import nltk
+    nltk.download('stopwords')
+    
+So, you have to execute this code once to be able to use default stop words for english texts.
+
+How To Run
+----------
+
+To run the code just execute the following command:
+
+    python3 check_similarity_of_strings.py
---
+++ fast_text_example/check_similarity_of_strings.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*- encoding=utf8 -*-
+
+from gensim.models.fasttext import FastText as FT_gensim
+from gensim.test.utils import datapath
+
+from nltk.corpus import stopwords
+
+
+# This is just full path to text file
+# with many different texts and topics,
+# you can set "corpus_file" equal to the path to
+# your own text file:
+corpus_file = datapath('lee_background.cor')
+
+model = FT_gensim(size=100)
+
+# build the vocabulary
+model.build_vocab(corpus_file=corpus_file)
+
+# train the model
+model.train(
+    corpus_file=corpus_file, epochs=model.epochs,
+    total_examples=model.corpus_count, total_words=model.corpus_total_words
+)
+
+sentence1 = 'Obama speaks to the media in Illinois'.lower().split()
+sentence2 = 'The president greets the press in Chicago'.lower().split()
+sentence3 = 'My dog is the the best boy ever'.lower().split()
+sentence4 = 'My cat hates me. She thinks she is a god'.lower().split()
+
+stopwords = stopwords.words('english')
+sentence1 = [w for w in sentence1 if w not in stopwords]
+sentence2 = [w for w in sentence2 if w not in stopwords]
+sentence3 = [w for w in sentence3 if w not in stopwords]
+sentence4 = [w for w in sentence4 if w not in stopwords]
+
+# If WM distance is lower, it means that sentences
+# are more similar. If the distance is higher, it means
+# that sentences are different.
+distance = model.wv.wmdistance(sentence1, sentence2)
+print(distance)
+
+distance = model.wv.wmdistance(sentence1, sentence3)
+print(distance)
+
+distance = model.wv.wmdistance(sentence3, sentence4)
+print(distance)
